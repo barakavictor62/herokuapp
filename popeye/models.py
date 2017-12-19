5b728644ae10e661816845f4ff78855031e28bcd
barakavictor62 <barakavictor62@gmail.com>
2017-12-19 17:25:12 +0300

    migrate

--- popeye/models.py
+++ popeye/models.py
@@ -12,14 +12,14 @@ class Profile(models.Model):
     country = models.CharField(max_length=255, blank=True)
     profile_picture = models.ImageField(upload_to='profile_picture', blank=True)
 
-"""@receiver(post_save, sender=User)
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()"""
+    instance.profile.save()
 
 class ContentWriting(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- popeyeweb/settings/production.py
+++ popeyeweb/settings/production.py
@@ -21,8 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '242k8t=tq72(549rjm%g$tay@%4q$ix=n5vy)l9qdo5%wbu*pw'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = '
+#242k8t=tq72(549rjm%g$tay@%4q$ix=n5vy)l9qdo5%wbu*pw'
+SECRET_KEY = os.environ.get('SECRET_KEY','242k8t=tq72(549rjm%g$tay@%4q$ix=n5vy)l9qdo5%wbu*pw')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 MAIL_USE_TLS=True
 EMAIL_HOST = 'smtp.gmail.com'
