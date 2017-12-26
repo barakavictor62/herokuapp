b723fc99aca9b62d23d6079b525f33b33f0b24a4
barakavictor62 <barakavictor62@gmail.com>
2017-12-26 23:44:05 +0300

    braintree

--- popeye/views.py
+++ popeye/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from popeye.forms import SignupForm, CheckOutForm, ContentRequestForm,EmailForm, WebsiteRequestForm, ProfileInfo,PasswordChange, UserChange, resetForm
 from .models import Profile, User, ContentWriting, WebsiteBuilding
+from django.conf import settings
 import re
 import braintree
 
