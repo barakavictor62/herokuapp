d2dc22b90bc602c8bafaebaa007f897325846b07
barakavictor62 <barakavictor62@gmail.com>
2017-12-28 06:20:30 +0300

    drop in ui

--- popeye/views.py
+++ popeye/views.py
@@ -61,10 +61,10 @@ def mywallet(request):
     if request.method== 'POST':
         add_amount = CheckOutForm(request.POST)
         if add_amount.is_valid():
-            """result = braintree.PaymentMethod.create({
-                "customer_id":request.user.id,
+            result = braintree.PaymentMethod.create({
+                "customer_id": "12345",
                 "payment_method_nonce": nonce_from_the_client
-                })"""
+                })
             my_nonce = braintree.PaymentMethodNonce.create(add_amount.cleaned_data['Client_Token'])
             result = braintree.Transaction.sale({
                 "customer_id": request.user.id,
@@ -84,8 +84,7 @@ def mywallet(request):
                 format(result.message)
     else:
         client_token = braintree.ClientToken.generate()
-        my_nonce = braintree.PaymentMethodNonce.create(client_token)
-        add_amount = CheckOutForm(initial={"Client_Token":client_token, "Payment_Method_Nonce":my_nonce})
+        add_amount = CheckOutForm(initial={"Client_Token":client_token})
         return render(request, "mywallet.html", {"me_articles":me_articles, "sum_total":sum, "add_amount": add_amount})
 
 def pricing(request):
