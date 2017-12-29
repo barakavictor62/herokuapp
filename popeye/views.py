2863ba45685b92e546d7fdfa3f64c6cb0eb9828a
barakavictor62 <barakavictor62@gmail.com>
2017-12-29 10:17:12 +0300

    dopin selector

--- popeye/views.py
+++ popeye/views.py
@@ -73,8 +73,10 @@ def mywallet(request):
                 })
             if result.is_success:
                 return redirect('/mywallet')
-   		    else:
-    		    return render(request, 'checkout_error.html', {})
+            else:
+                return render(request, 'checkout_error.html', {})
+       """ else:
+            return render(request, 'checkout_error.html', {})"""
     else:
         client_token = braintree.ClientToken.generate()
         add_amount = CheckOutForm(initial={"Client_Token":client_token})
