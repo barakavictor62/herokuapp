e7079571a5fb08d7d1d90e242a9b1e10e61ea83e
barakavictor62 <barakavictor62@gmail.com>
2017-12-30 18:20:13 +0300

    dopin selector

--- static/js/base-profile.js
+++ static/js/base-profile.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
       e.preventDefault();
     $('#wrapper').toggleClass('menuDisplayed');
     $("#page-content-wrapper").toggleClass("col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 col-lg-11 ");
-
-  });
+});
     $("#id_article_cost").val("$"+(($("#id_word_count").val())*0.016).toFixed(2));
 
     $("#id_word_count").change(function () {
