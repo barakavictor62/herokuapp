f21157ba1c94998df256b64c5d2696e367b39fe2
barakavictor62 <barakavictor62@gmail.com>
2017-12-30 14:08:45 +0300

    dopin selector

--- popeye/views.py
+++ popeye/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from popeye.forms import SignupForm, CheckOutForm, ContentRequestForm,EmailForm, WebsiteRequestForm, ProfileInfo,PasswordChange, UserChange, resetForm
+from popeye.forms import SignupForm, CheckOutForm,ContentRequestForm, ContentRequestForm,EmailForm,AnonWebsiteRequestForm, WebsiteRequestForm, ProfileInfo,PasswordChange, UserChange, resetForm
 from .models import Profile, User, ContentWriting, WebsiteBuilding
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
