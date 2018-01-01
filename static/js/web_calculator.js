921dbe47e6c1f80bcd143a77062a46de2674bb96
barakavictor62 <barakavictor62@gmail.com>
2018-01-01 03:28:39 +0300

    pricing

--- static/js/web_calculator.js
+++ static/js/web_calculator.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
         var web_cost = 0;
+        var obj = [];
         $("input[type=radio]").click(function(){
-            web_cost += Number($(this).val());
-            $(".website-price").text(web_cost);
+                var name = $(this).attr("name");
+                var hel_val = $(this).val();
+                var clicked_element = {name_el:name, el_hel_val:hel_val};
+                 obj.push(clicked_element);
+                 alert(obj);
         });
     });
