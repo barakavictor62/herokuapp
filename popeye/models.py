7f9936fb96057c3c31856521ad45c2b50e5750f0
barakavictor62 <barakavictor62@gmail.com>
2018-01-04 23:00:12 +0300

    Revert "first commit"
    This reverts commit 101994d432125288fdde4be3237eabf06ea83bc1.

---
+++ popeye/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+#User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0])
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=255, blank=True)
+    country = models.CharField(max_length=255, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_picture', blank=True)
+    Balance = models.CharField(max_length=255, default=0)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class ContentWriting(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    word_count = models.IntegerField()
+    article_cost = models.CharField(max_length=20)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    is_done = models.IntegerField(default=0)
+    is_paid = models.IntegerField(default=0)
+
+class AnonContentWriting(models.Model):
+    email = models.EmailField(max_length=255)
+    topic = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    word_count = models.IntegerField()
+    article_cost = models.CharField(max_length=20)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    is_done = models.IntegerField(default=0)
+    is_paid = models.IntegerField(default=0)
+
+class WebsiteBuilding(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    title = models.CharField(max_length=255, blank=True)
+    navigation_contents = models.CharField(max_length=500)
+    website_cost = models.CharField(max_length=20)
+    additional_instructions = models.CharField(max_length=1000)
+    is_done = models.IntegerField(default=0)
+    is_paid = models.IntegerField(default=0)
+
+class AnonWebsiteBuilding(models.Model):
+    email = models.EmailField(max_length=255)
+    company = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    title = models.CharField(max_length=255, blank=True)
+    navigation_contents = models.CharField(max_length=500)
+    website_cost = models.CharField(max_length=20)
+    additional_instructions = models.CharField(max_length=1000)
+    is_done = models.IntegerField(default=0)
+    is_paid = models.IntegerField(default=0)
+
