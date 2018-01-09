07aba3f33401c898a6239c801259014149b38388
barakavictor62 <barakavictor62@gmail.com>
2018-01-10 00:17:45 +0300

    close

--- static/js/base-profile.js
+++ static/js/base-profile.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
       e.preventDefault();
     $('#wrapper').toggleClass('menuDisplayed');
     $("#page-content-wrapper").toggleClass("col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 col-lg-11 ");
-});
+    });
+    
     $("#id_article_cost").val("$"+(($("#id_word_count").val())*0.016).toFixed(2));
 
     $("#id_word_count").change(function () {
@@ -17,11 +18,9 @@ $(document).ready(function() {
         $(this).siblings().find("a").removeClass("active");
         $(this).find("a").addClass("active");
         $("."+$(this).attr("id")).css({"display":"block"}).siblings().hide();
-
-
-    })
+    });
     $("#close-btn").click(function(){
-        $("#sidnav").width(0);
+        alert("hello");
     });
 
 });
